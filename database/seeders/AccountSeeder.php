f05584442d3dd21c826d784958e43344926b21fa
Lourdilene Souza <lourdilene.souza@gmail.com>
2021-07-31 22:34:15 -0300

    Criaçao da migration e seeder de account

---
+++ database/seeders/AccountSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use Illuminate\Database\Seeder;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Account::create([
+            'number' => '1212',
+            'balance' => '100.00'
+        ]);
+    }
+}
